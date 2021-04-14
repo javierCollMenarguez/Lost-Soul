6ddd605cc655d46d5a278586f21c7457d38b97e5
Javier Coll <jcollmenarguez@gmai.com>
2021-04-14 10:23:15 +0100

    first commit

---
+++ js web tech/input.js
@@ -0,0 +1,12 @@
+function fn1()
+    {
+        var str = document.getElementById("text1").value;
+        alert("Your review has been stored thank you!");
+       
+    }
+    function fn2()
+    {
+        var str = document.getElementById("text1").value;
+        alert("wellcome "+str);
+       
+    }
